b8e615cf263585ecdeb5a76debb9467bb55fefcb
IGuijarroJ <iguijarro3@gmail.com>
2022-05-21 10:29:39 +0200

    Unificando formularios con select

--- frontend/src/app/nuevamaletabarco/barcoform/barcoform.component.ts
+++ frontend/src/app/nuevamaletabarco/barcoform/barcoform.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MaletaBarco } from 'src/app/nuevamaleta/models/maleta-barco';
+import { MaletaBarcoImpl } from 'src/app/nuevamaleta/models/maleta-barco-impl';
+import { MaletaCabina } from 'src/app/nuevamaleta/models/maleta-cabina';
+import { MaletaCabinaImpl } from 'src/app/nuevamaleta/models/maleta-cabina-impl';
 @Component({
   selector: 'app-barcoform',
   templateUrl: './barcoform.component.html',
@@ -10,9 +13,11 @@ export class BarcoformComponent implements OnInit {
 
   constructor( ) {}
   public maletasBarco: MaletaBarco[] = [];
-  maletaBarco: MaletaBarco = { id: '', fechaRecogida: '', peso: 0 };
+  maletaBarco: MaletaBarco = new MaletaBarcoImpl;
+  maletaCabina: MaletaCabina = new MaletaCabinaImpl;
   creada = false;
   mensaje = '';
+  formulario: number = 0;
 
   ngOnInit(): void {}
   mostrarAyuda() {
